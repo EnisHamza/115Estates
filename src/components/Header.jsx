d6eaede9c47978e47892e235af19890b38640419
Enis Hamza <eniss.hamza@gmail.com>
2024-10-13 20:01:03 +0200

    Worked on Profile Page and fixed header menu between Profile and Sign In

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../images/logo.png";
 import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { RxDropdownMenu } from "react-icons/rx";
 import { FaHome } from "react-icons/fa";
 import { MdLocalOffer } from "react-icons/md";
 import { PiSignInBold } from "react-icons/pi";
+import { CgProfile } from "react-icons/cg";
 
 function Header() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [pageState, setPageState] = useState("Sign in");
+  const [user, setUser] = useState(null);
+
   const location = useLocation();
   const navigate = useNavigate();
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setPageState(user ? "Profile" : "Sign in");
+    });
+
+    return () => unsubscribe(); // Cleanup on unmount
+  }, [auth]);
 
   function pathMathRoute(route) {
-    return location.pathname === route;
+    return route === location.pathname;
   }
 
   const toggleDropdown = () => {
@@ -31,7 +46,6 @@ function Header() {
           />
         </div>
 
-        {/* Regular Navigation for Small Screens */}
         <nav className="flex sm:hidden">
           <ul className="flex space-x-10 mr-12">
             <li
@@ -52,13 +66,13 @@ function Header() {
             </li>
             <li
               className={`py-2 text-red-500 border-b-[3px] font-semibold text-sm cursor-pointer ${
-                pathMathRoute("/signIn")
+                pathMathRoute("/signIn") || pathMathRoute("/profile")
                   ? "text-red-500 border-b-red-600"
                   : "text-white border-b-transparent"
               }`}
-              onClick={() => navigate("/signIn")}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {pageState}
             </li>
           </ul>
         </nav>
@@ -102,15 +116,21 @@ function Header() {
                 </li>
                 <li
                   className={`py-2 flex flex-row text-white font-semibold text-sm cursor-pointer ${
-                    pathMathRoute("/signIn") ? "text-red-500" : "text-white"
+                    pathMathRoute("/signIn") || pathMathRoute("/profile")
+                      ? "text-red-500"
+                      : "text-white"
                   }`}
                   onClick={() => {
-                    navigate("/signIn");
+                    navigate("/profile");
                     setIsDropdownOpen(false);
                   }}
                 >
-                  <PiSignInBold className="mx-1" />
-                  Sign In
+                  {user ? (
+                    <CgProfile className="mx-1" />
+                  ) : (
+                    <PiSignInBold className="mx-1" />
+                  )}
+                  {pageState}
                 </li>
               </ul>
             </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,134 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getAuth, updateProfile } from "firebase/auth";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import bg from "../images/bg.png";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
 
 function Profile() {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+  }
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //update display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // update name in the firestore
+
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Profile details updated");
+    } catch (error) {
+      toast.error("Could not update the profile details");
+    }
+  }
+
+  return (
+    <div
+      className="w-full h-screen"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        className="p-20 border-4 rounded shadow-lg text-white"
+        style={{
+          background: "rgba(26, 35, 101, 0.5)", // Semi-transparent background
+          backdropFilter: "blur(12px)", // Apply blur
+        }}
+      >
+        <h2 className="flex flex-row justify-center mb-12 text-2xl font-bold">
+          My Profile
+        </h2>
+        <form>
+          {/* Name Input */}
+
+          <input
+            type="text"
+            id="name"
+            value={name}
+            disabled={!changeDetail}
+            onChange={onChange}
+            className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+              changeDetail && "bg-red-200 focus:bg-red-200"
+            }`}
+          />
+
+          {/* Email Input */}
+
+          <input
+            type="email"
+            id="email"
+            value={email}
+            disabled
+            className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+          />
+
+          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+            <p className="flex items-center ">
+              Want to change name?
+              <span
+                onClick={() => {
+                  changeDetail && onSubmit();
+                  setChangeDetail((prevState) => !prevState);
+                }}
+                className="text-red-600 hover:text-red-700 font-bold transition ease-in-out duration-200 ml-1 cursor-pointer"
+              >
+                {changeDetail ? "Apply change" : "Edit"}
+              </span>
+            </p>
+            <p
+              onClick={onLogout}
+              className="text-amber-400 hover:text-amber-800 font-bold text-base transition duration-200 ease-in-out cursor-pointer"
+            >
+              Sign Out
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
